42346d7c384af8a4aa1c766ca268ea6e682100a2
HuyHo1405 <hohuytencent@gmail.com>
2025-06-06 07:10:03 +0700

    Set up skeleton DAO and DTO model class for upcoming features

---
+++ Assignment/src/java/model/dao/UserDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.dao;
+
+/**
+ * Status: Chờ thực hiện
+ * Người thực hiện: [...........]
+ * Ngày bắt đầu: [...........]
+ * viết các CRUD cần thiết 
+ */
+public class UserDAO {
+    
+}
---
+++ Assignment/src/java/model/dto/CartDTO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.dto;
+
+/**
+ * Status: Chờ thực hiện
+ * Người thực hiện: [...........]
+ * Ngày bắt đầu: [...........]
+ * viết set up các field cho class này
+ */
+public class CartDTO {
+    
+}
